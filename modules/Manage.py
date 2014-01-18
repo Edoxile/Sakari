c3dc9e0530cb5766fe568a30d393225564d0c7cb
windwarrior <lennartbuit@gmail.com>
2014-01-19 00:08:10 +0100

    itertools.chain.from_iterable should be able to handle the multidimentional array

--- modules/Manage.py
+++ modules/Manage.py
@@ -36,7 +36,7 @@ class Manage(AuthModule):
         self.bot.die()
 
     def load(self, c, e, args):
-        modules = itertools.chain([n.split(',') for n in args])
+        modules = itertools.chain.from_iterable([n.split(',') for n in args])
         for m in modules:
             try:
                 print(m)
