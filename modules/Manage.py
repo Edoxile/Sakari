40b796d16627ad2fb60456527b25b967c7fe8469
windwarrior <lennartbuit@gmail.com>
2014-01-19 01:08:44 +0100

    Fix typo

--- modules/Manage.py
+++ modules/Manage.py
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 from modules.AuthModule import AuthModule, get_target
-from exception import SakariException
+from exceptions import SakariException
 
 import itertools
 import threading
