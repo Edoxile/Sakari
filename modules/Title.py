2e6bdae1d5e09ce21e6ef78e8954d11bd903f787
Edoxile <koen_75_@hotmail.com>
2014-01-21 15:58:30 +0100

    Added Title module

--- modules/Title.py
+++ modules/Title.py
@@ -13,17 +13,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-from modules.Module import Module
+from modules.Module import Module, get_target
 import re
+from urllib import request
+import magic
 
 __author__ = 'Edoxile'
 
 
 class Title(Module):
-
     def __init__(self, b):
         super().__init__(b)
         self.re_title = re.compile("((?:https?://|www\.)[-~=\\\/a-zA-Z0-9\.:_\?&%,#\+]+)")
+        self.re_html = re.compile("<title>(.+?)</title>")
 
     def get_commands(self):
         return []
@@ -34,7 +36,14 @@ class Title(Module):
         ]
 
     def get_title(self, c, e, msg):
-        m = self.re_title.match(msg)
-        return
+        m = self.re_title.search(msg)
         if m:
-            pass
+            url = request.urlopen(m.group(1))
+            data = url.read(1024)
+            info = magic.from_buffer(data).decode('utf-8')
+            if 'HTML document' in info:
+                t = self.re_html.search(data)
+                if t:
+                    c.privmsg(get_target(c, e), "[ {} ]".format(t.group(1)))
+            else:
+                c.privmsg(get_target(c, e), "[ {} ]".format(info))
