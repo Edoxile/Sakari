450e88c4617d436f799eb184af26d7380f951495
Edoxile <koen_75_@hotmail.com>
2014-01-28 18:00:42 +0100

    Add space in stead of newline

--- modules/Wiki.py
+++ modules/Wiki.py
@@ -40,14 +40,14 @@ class Wiki(Module):
         p = wikipedia.page(' '.join(args[1:]))
         if p:
             c.privmsg(get_target(c, e),
-                      '\x02{}\x0f - {} [ {} ]'.format(p.title, smart_truncate(p.summary.replace('\n', '')), p.url))
+                      '\x02{}\x0f - {} [ {} ]'.format(p.title, smart_truncate(p.summary.replace('\n', ' ')), p.url))
         wikipedia.set_lang(self.bot.config.get('wikipedia', 'lang'))
 
     def search(self, c, e, args):
         p = wikipedia.page(' '.join(args))
         if p:
             c.privmsg(get_target(c, e),
-                      '\x02{}\x0f - {} [ {} ]'.format(p.title, smart_truncate(p.summary.replace('\n', '')), p.url))
+                      '\x02{}\x0f - {} [ {} ]'.format(p.title, smart_truncate(p.summary.replace('\n', ' ')), p.url))
 
     def random(self, c, e, args):
         while True:
@@ -55,7 +55,7 @@ class Wiki(Module):
                 p = wikipedia.page(wikipedia.random())
                 if p:
                     c.privmsg(get_target(c, e),
-                              '\x02{}\x0f - {} [ {} ]'.format(p.title, smart_truncate(p.summary.replace('\n', '')),
+                              '\x02{}\x0f - {} [ {} ]'.format(p.title, smart_truncate(p.summary.replace('\n', ' ')),
                                                               p.url))
                     break
             except DisambiguationError:
